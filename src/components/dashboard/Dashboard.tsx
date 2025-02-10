3d3d51d77425329c7b3140df3bcc27e566076dd2
Ankush307 <ankushdhull966@gmail.com>
2025-02-10 16:46:05 +0530

    add meta tag

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -34,7 +34,7 @@ const Dashboard = () => {
         <div className="flex min-h-screen w-full md:pl-[300px] relative">
             <div className={`flex flex-col py-10 px-5 bg-black z-10 text-white w-[300px] fixed top-0 left-0 min-h-screen justify-between max-md:w-full transition-all duration-300 ${open === true ? "max-md:left-0" : "max-md:-left-full"}`}>
                 <div className="flex flex-col gap-2 relative">
-                    <h1 className="mb-3 text-center text-4xl font-semibold">Dashboard</h1>
+                    <h2 className="mb-3 text-center text-4xl font-semibold">Dashboard</h2>
                     {DASHBOARD_BUTTON_LIST.map((item, index) => (
                         <Link href={`/dashboard?page=${item.toLowerCase().replace(" ", "-")}`} key={index} onClick={() => setOpen(false)} className={`${page === item.toLowerCase().replace(" ", "-") && "bg-white text-black"} py-2 px-3 rounded-lg cursor-pointer hover:bg-white/50 transition-all duration-300 hover:text-black`}>{item}</Link>
                     ))}
@@ -43,12 +43,13 @@ const Dashboard = () => {
                 </button>
             </div>
             <div className="w-full pt-20">
-                <div className={`${open === true ? "!bg-transparent" : ""} bg-blue-500 max-md:flex max-md:items-center max-md:gap-5 min-h-20 py-5 px-3 w-full z-20 fixed top-0`}>
+                <div className="bg-blue-500 max-md:flex max-md:items-center max-md:gap-5 min-h-20 py-5 px-3 w-full z-20 fixed top-0">
                     <button onClick={handleOpen} className="md:hidden max-md:size-6 relative max-md:flex max-md:justify-between max-md:flex-col overflow-hidden">
                         <span className={`flex w-6 h-0.5 transition-all duration-300 bg-white ${open === true ? "translate-x-10" : ""}`}></span>
                         <span className={`flex w-6 h-0.5 transition-all duration-300 relative bg-white after:absolute after:w-full after:h-full after:bg-white after:left-0 after:top-0 after:transition-all after:duration-300 ${open === true ? "rotate-45 after:rotate-90" : ""}`}></span>
                         <span className={`flex w-6 h-0.5 transition-all duration-300 bg-white ${open === true ? "-translate-x-10" : ""}`}></span>
                     </button>
+                    <h2 className="text-white font-semibold font-inter text-3xl max-md:text-2xl">Welcome to Dashboard</h2>
                 </div>
                 {page === "button-1" ? (
                     <QuestionOne />
